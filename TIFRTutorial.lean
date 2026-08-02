eb8a6e2171612aa327588aa1dd360abc62502fa1
Sidharth Hariharan <fundamental.theor3m@gmail.com>
2026-08-02 23:35:28 +0530

    run `mk_all`

--- TIFRTutorial.lean
+++ TIFRTutorial.lean
@@ -1,4 +1,6 @@
 module  -- shake: keep-all --deprecated_module: ignore
 
+public import TIFRTutorial.Definitions.Demo
+public import TIFRTutorial.Definitions.Exercises
 public import TIFRTutorial.PropLog.Demo
 public import TIFRTutorial.PropLog.Exercises
