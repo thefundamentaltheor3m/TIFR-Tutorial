e7576acbe65acb40929af5fdbdb585f3776df4ad
Sidharth Hariharan <fundamental.theor3m@gmail.com>
2026-08-02 23:34:54 +0530

    add stuff about definitions

---
+++ TIFRTutorial/Definitions/Exercises.lean
@@ -0,0 +1,38 @@
+/-
+Copyright (c) 2026 Sidharth Hariharan. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Sidharth Hariharan
+-/
+module
+
+public import Mathlib
+
+@[expose] public section
+
+namespace MyDefs
+
+section task_1
+
+/-!
+# Task 1
+
+Using the example of the `Subgroup` type, define the types `Submodule` and `Ideal`.
+
+Hint: Scalar multiplication is abbreviated `SMul`. The symbol is • (achieved by typing `\smul`).
+-/
+
+end task_1
+
+section task_2
+
+/-!
+# Task 2
+
+State and prove that the natural numbers (under addition) are a submonoid of the integers.
+
+State and prove the corresponding result under multiplication.
+-/
+
+end task_2
+
+end MyDefs
