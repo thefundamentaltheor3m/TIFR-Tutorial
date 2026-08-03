fd9b55881020f658d59313c55eb231bfb3dd1e22
Sidharth Hariharan <64404030+thefundamentaltheor3m@users.noreply.github.com>
2026-08-03 13:53:23 +0530

    Session 1 (#4)
    * first half
    
    * second half
    
    * add tauto and wrap up

--- TIFRTutorial/PropLog/Demo.lean
+++ TIFRTutorial/PropLog/Demo.lean
@@ -5,6 +5,8 @@ Authors: Sidharth Hariharan
 -/
 module
 
+public import Mathlib
+
 @[expose] public section
 
 variable (P Q R S T : Prop)
@@ -16,18 +18,114 @@ We're going to work on this section live, as a way of demonstrating how to write
 and proofs.
 -/
 
+def my_prop : Prop := Nat.Prime 2
+
+def my_prop2 : Prop := Nat.Prime 57
+
+lemma my_prop_is_true : my_prop := by -- Nat.prime_two
+  sorry
+
+lemma my_prop2_is_true : my_prop2 := by
+  sorry
+
+lemma basic1 : P → P := fun h ↦ h
+
+example : P → P := by
+  intro (h : P)
+  exact h
+
+example : P → (Q → P) :=
+  -- intro (h : P)
+  -- intro h2
+  -- exact h
+  fun h ↦ (fun _ ↦ h)
+
+example (h : P) (k : P) : h = k := rfl
+
+example (h : P) (k : P) : h ≠ k := by
+  -- This is false
+  sorry
+
+example : P → (Q → P) := by
+  intro h
+  intro h2
+  exact h
+
+example : ¬ my_prop2 := by
+  intro (h : my_prop2)
+  sorry
+
 end basic
 
 section connectives
 
 example : P ∧ Q → Q ∧ P := by
-  sorry
+  intro h'
+  obtain ⟨h, h2⟩ := h'
+  constructor
+  · exact h2
+  · exact h
+
+example : P ∧ Q → Q ∧ P := by
+  intro h
+  constructor
+  · exact h.right
+  · exact h.left
+
+example : P ∧ Q → Q ∧ P := by
+  intro ⟨(h : P), (h2 : Q)⟩
+  constructor
+  · exact h2
+  · exact h
+
+example : P ∧ Q → Q ∧ P := by
+  rintro ⟨(h : P), (h2 : Q)⟩
+  constructor
+  · exact h2
+  · exact h
 
 example : P ∨ Q → Q ∨ P := by
+  -- tauto
+  intro (h : P ∨ Q)
+  cases h with
+  | inl tifr =>
+    right
+    exact tifr
+  | inr iitb =>
+    left
+    exact iitb
+
+example : P ∨ Q ∨ R → Q ∨ P := by
+  intro h
+  rcases h with hP | hQ | hR
+  · right
+    exact hP
+  · left
+    exact hQ
+  · sorry
+
+example : ¬ (P ∨ Q ∨ R → Q ∨ P) := by
+  -- push Not
   sorry
 
+example : P ∨ Q ∨ R → Q ∨ P := by -- same as P ∨ (Q ∨ R) → Q ∨ P
+  intro h
+  cases h with
+  | inl tifr =>
+    right
+    exact tifr
+  | inr iit =>
+    cases iit with
+    | inl iitb =>
+      left
+      exact iitb
+    | inr iitd =>
+      sorry
+
 example : (P ∧ Q) ∧ R ↔ P ∧ (Q ∧ R) := by
-  sorry
+  constructor
+  · sorry
+  · sorry
 
 example : (P ∧ Q → R) ↔ (P → Q → R) := by
   sorry
