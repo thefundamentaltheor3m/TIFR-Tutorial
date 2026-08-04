53d86da71b14d78f15d5402f588bd93128dbafa6
Sidharth Hariharan <fundamental.theor3m@gmail.com>
2026-08-04 11:47:04 +0530

    second half

--- TIFRTutorial/Definitions/Demo.lean
+++ TIFRTutorial/Definitions/Demo.lean
@@ -9,9 +9,44 @@ public import Mathlib
 
 @[expose] public section
 
+section as_structure
+
+structure MyGroup where
+  carrier : Type*
+  mul : carrier → carrier → carrier
+  inv : carrier → carrier
+  id : carrier
+  mul_assoc : ∀ x y z : carrier, mul (mul x y) z = mul x (mul y z)
+  mul_id : ∀ x : carrier, mul x id = x
+  id_mul : ∀ x : carrier, mul id x = x
+  mul_inv : ∀ x : carrier, mul x (inv x) = id
+  inv_mul : ∀ x : carrier, mul (inv x) x = id
+
+structure MyCommGroup extends MyGroup where
+  mul_comm : ∀ x y : carrier, mul x y = mul y x
+
 namespace MyGroup
 
-section as_structure
+instance instNonempty (G : MyGroup) : Nonempty G.carrier := ⟨G.id⟩
+
+def MyIntegers : MyCommGroup where
+  carrier := ℤ
+  mul := Int.add
+  inv := Int.neg
+  id := 0
+  mul_assoc := sorry
+  mul_id := sorry
+  id_mul := sorry
+  mul_inv := sorry
+  inv_mul := sorry
+  mul_comm := sorry
+
+#check MyCommGroup.toMyGroup
+
+instance instMyIntegersNonempty : Nonempty (MyIntegers.carrier) :=
+  instNonempty MyIntegers.toMyGroup
+
+end MyGroup
 
 end as_structure
 
@@ -19,11 +54,20 @@ section as_prop
 
 end as_prop
 
-end MyGroup
-
 #check Group
 #check Subgroup
 #check Set
+
+#check TopologicalSpace
+#check MetricSpace
+
+-- variable (X : Type*) [MetricSpace X]
+
+-- #synth TopologicalSpace X
+
+-- variable (k : Type*) [NormedField k] [SeminormedAddCommGroup X] [NormedSpace k X]
+
+-- example : Nonempty X := sorry
 
 section subgroups
 
@@ -46,3 +90,17 @@ example : 2 ∈ myEvens := by
 example : 3 ∈ myEvensSubset := by sorry
 
 end subgroups
+
+variable (E F : Type)
+
+def IsRadial [Norm E] (f : E → F) : Prop := f.FactorsThrough (‖·‖ : E → ℝ)
+
+-- variable [inst1 : Norm E] [inst2 : Norm E]
+
+-- example : @IsRadial E E inst2 (id (α := E)) := by
+--   have otherOne : @IsRadial E E inst1 id := sorry
+--   exact otherOne
+--   sorry
+
+instance instZ5Group : AddGroup (ZMod 5) where
+  neg_add_cancel := by simp
